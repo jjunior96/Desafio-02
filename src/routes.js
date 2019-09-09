7dc6a7094541b89ad650ae9fc4ddc54001257760
Junior Alves <jjalves96@gmail.com>
2019-09-09 05:55:47 -0300

    Update criado

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionCrontroller from './app/controllers/SessionsController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 
 routes.post('/sessions', SessionCrontroller.store);
 
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 export default routes;
